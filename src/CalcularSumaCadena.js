32ea02b4178bf99cc69cb02c661459d8c56e9679
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-10-02 13:27:36 -0200

    refactoring funciones

--- src/CalcularSumaCadena.js
+++ src/CalcularSumaCadena.js
@@ -1,13 +1,27 @@
+function obtenerListaValidaNumeros(cadena, posFin){
+  let subCadenaValida = cadena.slice(0,posFin);
+  let subListaValida = subCadenaValida.split();
+  return subListaValida;
+}
+
+function sumarLista(listaNumeros){
+  return listaNumeros.reduce((a, b) =>parseInt(a) + parseInt(b), 0);
+}
+
+function posicionCaracterDistintoDeDigito(cadena){
+  return cadena.search(/\D/);
+}
+
 function calcularSumaCadenas(cadena) {
   let suma = 0;
   let i =0;
   let numeros = cadena.split(/,|-/);
   while(i<numeros.length){
-    let posNoDigito=numeros[i].search(/\D/);
+    let posNoDigito = posicionCaracterDistintoDeDigito(numeros[i]);
     if( posNoDigito == 0) return suma;
-    let subCadenaValida = numeros[i].slice(0,posNoDigito);
-    let subLista = subCadenaValida.split();
-    if (posNoDigito>-1 ) return subLista.reduce((a, b) =>parseInt(a) + parseInt(b), 0);
+    let subListaValida = obtenerListaValidaNumeros(numeros[i], posNoDigito);
+    if (posNoDigito > 0 ) return sumarLista(subListaValida);
+
     let num = parseInt(numeros[i]);
     if(Number.isNaN(num)) return suma;
     suma += num;       
