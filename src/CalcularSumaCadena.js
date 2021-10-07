4560dfa17a358051f1fbe0746b4a4bd8a7acb571
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-10-07 09:10:11 -0200

    Refactoring: Cambio de nombres de variables completo

--- src/CalcularSumaCadena.js
+++ src/CalcularSumaCadena.js
@@ -1,19 +1,50 @@
 function calcularSumacadenaDeNumeross(cadenaDeNumeros) {
-  let suma = 0;
+  let sumaDeNumeros = 0;
   let contadorPosicion =0;
   let listaDeNumeros = obtenerListaNumeros(cadenaDeNumeros);
   while(contadorPosicion<listaDeNumeros.length){
-    let posNoDigito = posicionCaracterDistintoDeDigito(listaDeNumeros[contadorPosicion]);
-    if( posNoDigito == 0) return suma;
-    let subListaValida = obtenerListaValidaNumeros(listaDeNumeros[contadorPosicion], posNoDigito);
-    if (posNoDigito > 0 ) return suma + sumarLista(subListaValida);
+    let posicionSimboloNoValido = posicionCaracterDistintoDeDigito(listaDeNumeros[contadorPosicion]);
+    if( posicionSimboloNoValido == 0) return sumaDeNumeros;
+    let subListaValida = obtenerListaValidaNumeros(listaDeNumeros[contadorPosicion], posicionSimboloNoValido);
+    if (posicionSimboloNoValido > 0 ) return sumaDeNumeros + sumarLista(subListaValida);
 
-    let num = parseInt(listaDeNumeros[contadorPosicion]);
-    if(Number.isNaN(num)) return suma;
-    if(num <= 1000) suma += num;       
+    let numero = parseInt(listaDeNumeros[contadorPosicion]);
+    if(Number.isNaN(numero)) return sumaDeNumeros;
+    if(numero <= 1000) sumaDeNumeros += numero;       
     contadorPosicion++;   
   }
-  return suma;
+  return sumaDeNumeros;
+}
+
+function obtenerListaNumeros(cadenaDeNumeros){
+  let separadores = obtenerSeparadores(cadenaDeNumeros); 
+  let expresionSeparadora =  new RegExp(separadores);
+  cadenaDeNumeros = obtenercadenaDeNumerosValida(cadenaDeNumeros);
+  let listaNumeros = cadenaDeNumeros.split(expresionSeparadora);  
+  return listaNumeros;
+}
+
+function obtenerSeparadores(cadenaDeNumeros){
+  let separador = ",";
+  let separadores =`,|-`;
+  if(tieneDelimitador(cadenaDeNumeros)){
+    separador = cadenaDeNumeros.split("\n")[0];
+    separador = separador.replace("//[","");
+    separadores = buscarDelimitadores(separador);
+  }  
+  return separadores;
+}
+
+function buscarDelimitadores(cadenaDeNumerosSeparadora){
+  let matches= cadenaDeNumerosSeparadora.matchAll("]","")
+  let limiteInicial = 0;
+  let separadores = `,|-`;
+  for (const match of matches) {
+    let separador = cadenaDeNumerosSeparadora.slice(limiteInicial,match.index);
+    limiteInicial = match.index+2;
+    separadores = separadores += `|[${separador}]+`;
+  }
+  return separadores;
 }
 
 function obtenerListaValidaNumeros(cadenaDeNumeros, posicionFinal){
@@ -23,8 +54,8 @@ function obtenerListaValidaNumeros(cadenaDeNumeros, posicionFinal){
 }
 
 function sumarLista(listaNumeros){
-  listaNumeros = listaNumeros.filter(num=>num <= 1000)
-  return listaNumeros.reduce((a, b) =>parseInt(a) + parseInt(b), 0);
+  listaNumeros = listaNumeros.filter(numero=>numero <= 1000);
+  return listaNumeros.reduce((numero1, numero2) =>parseInt(numero1) + parseInt(numero2), 0);
 }
 
 function posicionCaracterDistintoDeDigito(cadenaDeNumeros){
@@ -41,37 +72,5 @@ function obtenercadenaDeNumerosValida(cadenaDeNumeros){
   }
   return cadenaDeNumeros;
 }
-
-function buscarDelimitadores(cadenaDeNumerosSeparadora){
-  let matches= cadenaDeNumerosSeparadora.matchAll("]","")
-  let limiteInicial = 0;
-  let separadores = `,|-`;
-  for (const match of matches) {
-    let sep = cadenaDeNumerosSeparadora.slice(limiteInicial,match.index);
-    limiteInicial = match.index+2;
-    separadores = separadores += `|[${sep}]+`;
-  }
-  return separadores;
-}
-
-function obtenerSeparadores(cadenaDeNumeros){
-  let separador = ",";
-  let separadores =`,|-`;
-  if(tieneDelimitador(cadenaDeNumeros)){
-    separador = cadenaDeNumeros.split("\n")[0];
-    separador = separador.replace("//[","");
-    separadores = buscarDelimitadores(separador);
-  }  
-  return separadores;
-}
-
-function obtenerListaNumeros(cadenaDeNumeros){
-  let separadores = obtenerSeparadores(cadenaDeNumeros); 
-  let expr =  new RegExp(separadores);
-  cadenaDeNumeros = cadenaDeNumeros = obtenercadenaDeNumerosValida(cadenaDeNumeros);
-  let listaNumeros = cadenaDeNumeros.split(expr);  
-  return listaNumeros;
-}
-
 
 export default calcularSumacadenaDeNumeross;
